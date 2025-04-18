72bba1af3329d03dda03dee5eb54f7a4229047ca
Yugalkishore-j <yugaljajawara@gmail.com>
2025-04-19 00:38:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGO_URI;
+require('dotenv').config({ path: './backend/.env' });
 
-const connectToMongo = async () => {
-  try {
-    await mongoose.connect(mongoURI);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-  }
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('connected successfully to mongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
 };
 
-module.exports = connectToMongo;
+module.exports = connectDB;
+
